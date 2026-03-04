a91939a7707e780d1bfe6b83b704043f4c58d211
Kendra <kendraaabellana@gmail.com>
2026-03-04 22:25:49 +0800

    layout structure and middleware

--- Abellana_Portfolio/routes/web.php
+++ Abellana_Portfolio/routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/projects', function () {
+    return view('projects');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
